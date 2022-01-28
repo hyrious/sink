bbc77c2d82794455a87257422cc5572c9293a2a6
hyrious <hyrious@outlook.com>
2022-01-28 10:51:13 +0800

    wip

--- src/yata.test.ts
+++ src/yata.test.ts
@@ -1,35 +1,74 @@
-import type { Item } from './yata'
-import { gen_uid, HEAD, insert, remove, store } from './yata'
+import type { Clock, Operation, UserId } from './yata'
+import { insert, remove } from './yata'
 
-const id1 = gen_uid('hello') // hello,
-const id2 = gen_uid('world') // world!
-const id3 = gen_uid('goodbye') // goodbye
-
-const actions = [
-  () => insert(store, id2, 'world!', id1, ''),
-  () => insert(store, id1, 'Hello, ', HEAD.id, ''),
-  () => remove(store, id1),
-  () => insert(store, id3, 'Goodbye, ', HEAD.id, id2),
-]
-
-function rand (n: number) {
-  return Math.floor(Math.random() * n)
+let doc: Operation[] = []
+const rand = (n: number) => (Math.random() * n) | 0
+const shuffle = <T>(array: T[]) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = rand(i + 1)
+    const temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+  return array
 }
+const print = () => doc
+  .filter(e => !e.isDeleted)
+  .map(({ userId, clock, content }) => `[${userId}:${clock} ${JSON.stringify(content)}]`).join('')
+const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const op_to_origin = (op: Operation): [UserId, Clock] => [op.userId, op.clock]
 
-actions.sort(() => rand(3) - 1).forEach(action => {
-  console.log(action.toString()); action()
-})
+class User {
+  clock: number
 
-// actions.forEach(action => {
-//   console.log(action.toString()); action()
-// })
+  constructor (readonly userId: string) {
+    this.clock = 1
+  }
 
-const out = []
-for (let p: Item | undefined = HEAD; p; p = p.right) {
-  if (p.deleted) {
-    out.push(`deleted(${JSON.stringify(p.contents)})`)
-  } else {
-    out.push(JSON.stringify(p.contents))
+  makeInsertOp (content: string, origin: [UserId, Clock] | null): Operation {
+    return {
+      userId: this.userId,
+      clock: this.clock++,
+      origin,
+      content,
+      isDeleted: false,
+    }
+  }
+
+  insert (i: Operation) {
+    console.log(`>> ${this.userId} insert ${i.content} at ${i.origin}`, print())
+    insert(i, doc)
+    console.log('=>', print())
+  }
+
+  delete (i: Operation) {
+    console.log(`>> ${this.userId} delete ${i.content}`, print())
+    remove(i)
+    console.log('=>', print())
   }
 }
-console.log(out.join(' <=> '))
+
+const users = Array.from({ length: 3 }, (_, i) => new User(`user${i + 1}`))
+const rand_op = () => {
+  const user = users[rand(users.length)]
+  if (doc.length && rand(2)) {
+    const i = doc[rand(doc.length)]
+    return () => user.delete(i)
+  } else {
+    const content = alphabet[rand(alphabet.length)].repeat(rand(10) + 1)
+    const origin = doc.length ? op_to_origin(doc[rand(doc.length)]) : null
+    const i = user.makeInsertOp(content, origin)
+    return () => user.insert(i)
+  }
+}
+
+const ops = Array.from({ length: 5 }, () => {
+  const op = rand_op()
+  op() // apply it once
+  return op
+})
+console.log('\nexpect', print(), '\n')
+
+doc = []
+shuffle(ops).forEach(op => op())
+console.log('\nactual', print())
--- src/yata.ts
+++ src/yata.ts
@@ -1,90 +1,89 @@
-export type Id = string
-export type Store = Map<Id, Item> & { pending: Map<Set<Id>, () => void> }
+// eslint-disable-next-line max-len
+// heavily based on that paper: https://www.researchgate.net/publication/310212186_Near_Real-Time_Peer-to-Peer_Shared_Editing_on_Extensible_Data_Types
+// also see: https://github.com/josephg/reference-crdts
 
-export interface Item {
-  id: Id
-  left?: Item
-  right?: Item
-  origin: Id
-  rightOrigin: Id
-  contents: string
-  deleted?: true
+// Each user has an unique ID.
+export type UserId = string
+
+// Each user gets an operation counter which gets incremented
+// every time a user creates an operation.
+export type Clock = number
+
+// Each operation has an unique identifier (userId, clock).
+// In YATA we have two operations running on a linked list: insert & delete.
+// An insert operation has:
+// (userId, clock) - the identifier
+// origin          - where it want to be inserted after
+// (left, right)   - where it is really inserted between
+// content         - the content, e.g. a character
+// isDeleted       - a boolean indicating if this operation is deleted
+export interface Operation {
+  userId: UserId
+  clock: Clock
+  origin: [UserId, Clock] | null // optimize: add `originRight` to narrow down the searching range
+  content: string
+  isDeleted: boolean
 }
 
-export function gen_uid (debug = '') {
-  return debug + '-' + Math.random().toString(36).slice(2, 6)
-}
-
-export const HEAD: Item = { id: gen_uid('head'), origin: '', rightOrigin: '', contents: '' }
-export const store = /* @__PURE__ */ Object.assign(
-  /* @__PURE__ */ new Map([[HEAD.id, HEAD]]), { pending: /* @__PURE__ */ new Map() }
-)
-
-export function insert (store: Store, id: Id, contents: string, origin: Id, rightOrigin: Id) {
-  const item: Item = { id, origin: '', rightOrigin: '', contents }
-  store.set(id, item)
-  if ((origin && !store.has(origin)) || (rightOrigin && !store.has(rightOrigin))) {
-    const depends = new Set<Id>([origin, rightOrigin].filter(e => e && !store.has(e)))
-    store.pending.set(depends, () => insert(store, id, contents, origin, rightOrigin))
+// Compare operations, figure out which is the left.
+function compareOpWithId (op: Operation, origin: [UserId, Clock] | null) {
+  if (origin === null) {
+    return 1 // always insert to the begin
+  } else if (op.userId === origin[0] && op.clock === origin[1]) {
+    return 0
+  } else if (op.userId === origin[0] && op.clock < origin[1]) {
+    return -1
   } else {
-    item.left = store.get(origin)
-    item.right = store.get(rightOrigin)
-    item.origin = origin
-    item.rightOrigin = rightOrigin
-    if ((!item.left && (!item.right || item.right.left)) ||
-      (item.left && item.left.right !== item.right)) {
-      let left = item.left
-      let o = left?.right
-      const conflicting = new Set<Item>()
-      const all = new Set<Item>()
-      while (o && o !== item.right) {
-        all.add(o)
-        conflicting.add(o)
-        if (item.origin === o.origin) {
-          if (o.id < item.id) {
-            left = o
-            conflicting.clear()
-          } else if (item.rightOrigin === o.rightOrigin) {
-            break
-          }
-        } else if (o.origin && all.has(store.get(o.origin) as Item)) {
-          if (!conflicting.has(store.get(o.origin) as Item)) {
-            left = o
-            conflicting.clear()
-          }
-        } else {
-          break
-        }
-        o = o.right
-      }
-      item.left = left
-    }
-    if (item.left) {
-      const right = item.left.right
-      item.right = right
-      item.left.right = item
-    }
-    if (item.right) {
-      item.right.left = item
-    }
-    if (store.pending) {
-      for (const depends of store.pending.keys()) {
-        depends.delete(id)
-        if (depends.size === 0) {
-          const action = store.pending.get(depends) as () => void
-          store.pending.delete(depends)
-          action()
-        }
-      }
-    }
+    return op.userId.localeCompare(origin[0])
   }
 }
 
-export function remove (store: Store, id: Id) {
-  const item = store.get(id)
-  if (!item) {
-    store.pending.set(new Set([id]), () => remove(store, id))
-  } else {
-    item.deleted = true
+// Slow search
+// optimize: use struct-store and binary search to find the operation faster
+function find_origin (origin: [UserId, Clock], doc: Operation[]) {
+  const exact = doc.findIndex(e => e.userId === origin[0] && e.clock === origin[1])
+  if (exact !== -1) {
+    return exact
   }
+  return Math.max(doc.findIndex(e => compareOpWithId(e, origin) >= 0) - 1, -1)
+}
+
+function compareIdWithId (id1: [UserId, Clock] | null, id2: [UserId, Clock] | null) {
+  if (id2 === null) {
+    return id1 === null ? 0 : -1
+  } else if (id1 === null) {
+    return 1
+  } else if (id1[0] === id2[0]) {
+    return id1[1] - id2[1]
+  } else {
+    return id1[0].localeCompare(id2[0])
+  }
+}
+
+// Insert `i` in a list of conflicting operations `doc`.
+export function insert (i: Operation, doc: Operation[]) {
+  let index = i.origin ? find_origin(i.origin, doc) : -1
+  // `i` want to insert at `left`.
+  let left = index
+  // Searching from `index` to the end of the list, resolve conflicts
+  for (; index >= 0 && index < doc.length; ++index) {
+    const o = doc[index]
+    // Rule 2: Search for the last operation that is to the left of `i`.
+    if (
+      (compareOpWithId(o, i.origin) < 0 || compareIdWithId(i.origin, o.origin) <= 0) &&
+      (compareIdWithId(o.origin, i.origin) !== 0 || o.userId.localeCompare(i.userId) < 0)
+    ) {
+      // Rule 1 and 3: If this formula is fulfilled, `i` is to the right of `o`.
+      left = index
+    } else if (compareIdWithId(i.origin, o.origin) > 0) {
+      // Breaking condition, Rule 1 is no longer satisfied.
+      break
+    }
+  }
+  // Insert `i` at `left`.
+  doc.splice(left + 1, 0, i)
+}
+
+export function remove (i: Operation) {
+  i.isDeleted = true
 }
